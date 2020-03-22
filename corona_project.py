4885c5e91f7a2b77a9773436696d0d225525a54b
Imran <imran789924@gmail.com>
2020-03-22 20:23:11 +0600

    Imporvements Done

--- corona_project.py
+++ corona_project.py
@@ -10,7 +10,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('/home/imran/PROGRAMS/Machine Learning/Practice/corona/data.csv')
+dataset = pd.read_csv('/home/imran/PROGRAMS/Machine Learning/Corona/corona/data.csv')
 
 dataset = dataset.drop(['Timestamp'], axis = 1)
 
@@ -29,7 +29,7 @@ X[:, 6] = le.fit_transform(X[:, 6])
 
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=10)
 
 
 from sklearn.linear_model import LogisticRegression
@@ -40,3 +40,16 @@ predictoin = regressor.predict(X_test)
 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, predictoin)
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Reds)
+plt.colorbar()
+classnames=['Not corona', 'corona ']
+tick_marks = np.arange(len(classnames))
+plt.xticks(tick_marks, classnames, rotation=70)
+plt.yticks(tick_marks, classnames)
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.show()
+
+
+from sklearn.metrics import accuracy_score
+accuracy_rate = accuracy_score(y_test, predictoin)
